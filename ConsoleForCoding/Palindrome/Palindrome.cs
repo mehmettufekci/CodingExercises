f05c27a25606773ac6a4bba69d3671eda96b2c88
Mehmet TUFEKCI <mtufekci1446@hotmail.com>
2023-10-09 00:31:28 +0300

    Palindrome

---
+++ ConsoleForCoding/Palindrome/Palindrome.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleForCoding.Palindrome;
+
+public class Palindrome
+{
+    public bool CheckPalindrome(string inputString)
+    {
+        int diziLength = (inputString.Length / 2);
+        bool sonuc = false;
+
+        if (string.IsNullOrEmpty(inputString))
+        {
+            sonuc = false;
+        }
+        else
+        {
+
+            if (diziLength < 1)
+            {
+                sonuc = true;
+            }
+            else
+            {
+
+                for (int i = 0; i < diziLength; i++)
+                {
+                    for (int j = inputString.Length - 1; j > diziLength; j--)
+                    {
+                        if (inputString[j] == inputString[i])
+                        {
+                            sonuc = true;
+                        }
+                        else
+                        {
+                            sonuc = false;
+                        }
+                    }
+                }
+            }
+        }
+        return sonuc;
+    }
+}
--- ConsoleForCoding/Program.cs
+++ ConsoleForCoding/Program.cs
@@ -1,5 +1,12 @@
-﻿using ConsoleForCoding.TakeCenturyFromYear;
+﻿using ConsoleForCoding.Palindrome;
+using ConsoleForCoding.TakeCenturyFromYear;
 
-CenturyFromYear ins = new();
-int year = 1905;
-Console.Write("This is the century:" + ins.GetCentury(year) + " of the year: " + year.ToString());
+//CenturyFromYear ins = new();
+//int year = 1905;
+//Console.Write("This is the century:" + ins.GetCentury(year) + " of the year: " + year.ToString());
+
+Palindrome palindrome = new();
+
+string inputString = "";
+
+Console.Write("This string: " + inputString + " plaindrome status: " + palindrome.CheckPalindrome(inputString));
