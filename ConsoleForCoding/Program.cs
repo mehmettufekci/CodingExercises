b9395781f2089e1776e0808676ca393c89bdeeb8
Mehmet TUFEKCI <mtufekci1446@hotmail.com>
2023-10-09 00:00:13 +0300

    Add project files.

---
+++ ConsoleForCoding/Program.cs
@@ -0,0 +1,5 @@
+﻿using ConsoleForCoding.TakeCenturyFromYear;
+
+CenturyFromYear ins = new();
+int year = 1905;
+Console.Write("This is the century:" + ins.GetCentury(year) + " of the year: " + year.ToString());
---
+++ ConsoleForCoding/TakeCenturyFromYear/CenturyFromYear.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleForCoding.TakeCenturyFromYear;
+
+public class CenturyFromYear
+{
+    public int GetCentury(int year)
+    {
+        return year % 100 == 0 ? year / 100 : year / 100 + 1;
+    }
+}
