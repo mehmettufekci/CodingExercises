ef24f27c8bda1ffc5e9dce0c501ca512ef08503c
Mehmet TUFEKCI <mtufekci1446@hotmail.com>
2023-10-11 00:18:12 +0300

    Adjacent Elements Product solution class added

---
+++ ConsoleForCoding/AdjacentElementsProduct/AdjacentElementsProduct.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleForCoding.AdjacentElementsProduct;
+
+public class AdjacentElementsProduct
+{
+    public int GetAdjacentElementLargestProduct(int[] inputArray)
+    {
+        int tempVal1, maxVal = -1000;
+        for (int i = 0; i < inputArray.Length; i++)
+        {
+            if (i < inputArray.Length - 1)
+            {
+                tempVal1 = inputArray[i] * inputArray[i + 1];
+                if (tempVal1 > maxVal)
+                {
+                    maxVal = tempVal1;
+                }
+            }
+        }
+        return maxVal;
+    }
+}
--- ConsoleForCoding/Program.cs
+++ ConsoleForCoding/Program.cs
@@ -1,12 +1,19 @@
-﻿using ConsoleForCoding.Palindrome;
+﻿using ConsoleForCoding.AdjacentElementsProduct;
+using ConsoleForCoding.Palindrome;
 using ConsoleForCoding.TakeCenturyFromYear;
 
+//Centruy From Year tests
 //CenturyFromYear ins = new();
 //int year = 1905;
 //Console.Write("This is the century:" + ins.GetCentury(year) + " of the year: " + year.ToString());
 
-Palindrome palindrome = new();
+//Palindrome tests
+//Palindrome palindrome = new();
+//string inputString = "abac";
+//Console.Write("This string: " + inputString + " plaindrome status: " + palindrome.CheckPalindrome(inputString));
 
-string inputString = "abac";
+//Adjacent Elements Product tests
 
-Console.Write("This string: " + inputString + " plaindrome status: " + palindrome.CheckPalindrome(inputString));
+AdjacentElementsProduct adjacentElements = new();
+int[] inputArray = { -23, 4, -3, 8, -12 };
+Console.Write("Adjacent Elements Product is: " + adjacentElements.GetAdjacentElementLargestProduct(inputArray));
